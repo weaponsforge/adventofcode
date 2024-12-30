0be1d07ef21e4dc17c1908c24c7116b3cb32b85e
weaponsforge <forgeweapons@gmail.com>
2024-12-30 22:52:07 +0800

    docs: update aoc file reader docs

--- src/utils/aocInputFile.ts
+++ src/utils/aocInputFile.ts
@@ -1,6 +1,14 @@
 // This code contains file input readers for common AoC input types.
 import { readFile } from './file.js'
 
+/**
+ * Definitions for the privimite types (`AOCOutputType`) in which to convert the AoC quiz input
+ * - `STRING` - String of text: `string`
+ * - `STRING_ARRAY`- Array of strings: `string[]`
+ * - `STRING_ARRAY_2D` - 2D array of strings: `string[][]`
+ * - `NUMBER_ARRAY` - Array of numbers: `number[]`
+ * - `NUMBER_ARRAY_2D` - 2D array of numbers: `number[][]`
+ */
 export enum AOC_OUTPUT_TYPE {
   STRING = 'string',
   STRING_ARRAY = 'string_array',
@@ -9,6 +17,10 @@ export enum AOC_OUTPUT_TYPE {
   NUMBER_ARRAY_2D = '2d_number_array'
 }
 
+/**
+ * Input parameters indicating details about the AoC quiz input file.
+ * @para,
+ */
 type FileInput = {
   /** @param filePath {string} Full file path to input file */
   filePath: string;
@@ -16,18 +28,22 @@ type FileInput = {
   delimiter?: string;
 }
 
-type Output = string | string[] | string[][] |
+/**
+ * Represents common primitive types in which to convert the AoC quiz input
+ */
+type AOCOutputType = string | string[] | string[][] |
   number[] | number[][]
 
 /**
- * Reads common AoC input text files.
- * @typedef param {FileInput} File input definitions
- * @param param.filePath {string} Full file path to an input text file
- * @param param.type {AOC_OUTPUT_TYPE} Type to convert the input text file
- * @param param.delimiter {string} String delimiter
+ * Reads common AoC input text files and converts them into one of `AOCOutputType` for data processing.
+ * @typedef {FileInput} param File input definitions
+ * @param {string} param.filePath Full file path to an input text file
+ * @param {AOC_OUTPUT_TYPE} param.type Type to convert the input text file. One of `AOC_OUTPUT_TYPE`.
+ * @param {string} param.delimiter String delimiter to `split()` between characters in the original text file. Defaults to none.
+ * @returns {AOCOutputType} Input text file converted into one of `AOCOutputType`
  * @throws {Error}
  */
-export const readAOCInputFile = (param: FileInput): Output => {
+export const readAOCInputFile = (param: FileInput): AOCOutputType => {
   const file = readFile(param.filePath)
   const delimiter = param?.delimiter ?? ''
 
