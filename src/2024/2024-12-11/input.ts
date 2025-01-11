0821e5d266e76f089431e335520df83706c6abb5
weaponsforge <forgeweapons@gmail.com>
2025-01-11 21:44:36 +0800

    chore: update 2024 day 11/14 inputs, #49

---
+++ src/2024/2024-12-11/input.ts
@@ -0,0 +1,5 @@
+// Quiz input random data.
+// Replace the input value with other values for testing
+
+export const inputSample: string = '125 17'
+export const inputQuiz: string = '2 34576 0 740884 60 103 1721 7405157'
--- src/2024/2024-12-14/main.ts
+++ src/2024/2024-12-14/main.ts
@@ -2,23 +2,20 @@ import { fileReader } from './lib/fileReader.js'
 import { calculateSafetyFactor } from './lib/safetyFactor.js'
 import { findEasterEgg } from './lib/findEasterEgg.js'
 
-const dataSample = fileReader('../input_sample.txt')
-const dataQuiz = fileReader('../input.txt')
-
-// Grid dimensions for the sample input
-const gridSample = { length: 7, width: 11 }
-
-// Grid dimensions for the randomized quiz input
-const gridQuiz = { length: 103, width: 101 }
-
 /**
  * Part 1/2 of the 2024-12-14 quiz
  * Counts the safety factor from robots after N seconds
  */
 const quiz20241214_01 = () => {
+  // Grid dimensions for the randomized quiz input
+  const gridSettings = { length: 7, width: 11 }
+
+  // Sample data. Replace with AoC quiz data.
+  const dataInput = fileReader('../input1.txt')
+
   const safetyFactor = calculateSafetyFactor({
-    data: dataSample,
-    gridMeta: gridSample,
+    data: dataInput,
+    gridMeta: gridSettings,
     seconds: 100,
     log: true
   })
@@ -31,9 +28,15 @@ const quiz20241214_01 = () => {
  * Counts the no. of seconds before robots display the easter egg
  */
 const quiz20241214_02 = () => {
+  // Grid dimensions for the randomized quiz input
+  const gridSettings = { length: 103, width: 101 }
+
+  // Random AoC auiz data
+  const dataInput = fileReader('../input.txt')
+
   const seconds = findEasterEgg({
-    data: dataQuiz,
-    gridMeta: gridQuiz,
+    data: dataInput,
+    gridMeta: gridSettings,
     seconds: 10000
     // Uncomment to print the xmas tree
     // log: true
